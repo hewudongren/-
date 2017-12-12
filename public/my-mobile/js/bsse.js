6accf1714710649cfc798973936482acf742afdc
小哥 <286739543.pp.com>
2017-12-12 22:09:21 +0800

    后台页面,与前台页面ok

---
+++ public/my-mobile/js/bsse.js
@@ -0,0 +1,20 @@
+//区域滚动
+mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false, //是否显示滚动条
+});
+//获得slider插件对象
+var gallery = mui('.mui-slider');
+gallery.slider({
+  interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+});
+//点击放回箭头,放回上一页
+$(".back").on("click",function(){
+      window.history.back()
+})
+//点击搜索按钮,放回搜索中心
+$(".search ").on("tap",function(){
+  window.location.href="./search.html"
+})
+
+
